c6ac66a349df71e615b878650573d5127df2339a
Carlos Bolanos <cbolanos@expediagroup.com>
2021-11-29 12:27:27 -0800

    ch3 simple jpa sboot testing

---
+++ src/test/java/guru/springframework/spring5webapp/persistance/BookRepositoryTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5webapp.persistance;
+
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ComponentScan(basePackages = { "guru.springframework.spring5webapp.bootstrap" })
+public class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // @Test // this test doesnt work because it didnt trigger the data initialize command runner, since this is just testing the jpa persistance layer, nice
+    // public void testBookRepository(){
+    // long count = bookRepository.count();
+    // assertThat(count).isGreaterThan(0);
+    // }
+
+    @Test
+    @Order(1)
+    @Commit
+    void testJpaTestSplice(){
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+        bookRepository.save(new Book("my book", "1234", "self"));
+
+        long countAfter = bookRepository.count();
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Test
+    @Order(2)
+    void testJpaTestSpliceTransaction(){
+        long count = bookRepository.count();
+        assertThat(count).isEqualTo(3);
+    }
+}
