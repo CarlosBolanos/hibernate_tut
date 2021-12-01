c4d476b5d45c392c8ff58bcf90efa82fb56380d0
Carlos Bolanos <cbolanos@expediagroup.com>
2021-12-01 10:51:01 -0800

    remove h2 database

--- src/test/java/guru/springframework/spring5webapp/unit/persistance/BookRepositoryTest.java
+++ src/test/java/guru/springframework/spring5webapp/unit/persistance/BookRepositoryTest.java
@@ -2,35 +2,23 @@ package guru.springframework.spring5webapp.unit.persistance;
 
 import guru.springframework.spring5webapp.domain.Book;
 import guru.springframework.spring5webapp.repositories.BookRepository;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Commit;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ComponentScan(basePackages = { "guru.springframework.spring5webapp.bootstrap" })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest {
 
     @Autowired
     BookRepository bookRepository;
 
-    // @Test // this test doesnt work because it didnt trigger the data initialize command runner, since this is just testing the jpa persistance layer, nice
-    // public void testBookRepository(){
-        // long count = bookRepository.count();
-        // assertThat(count).isGreaterThan(0);
-    // }
-
     @Test
-    @Order(1)
-    @Commit
     void testJpaTestSplice(){
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(4);
@@ -38,12 +26,5 @@ public class BookRepositoryTest {
 
         long countAfter = bookRepository.count();
         assertThat(countBefore).isLessThan(countAfter);
-    }
-
-    @Test
-    @Order(2)
-    void testJpaTestSpliceTransaction(){
-        long count = bookRepository.count();
-        assertThat(count).isEqualTo(5);
     }
 }
